953081c7354806f7a2936cd170be6f7231db7856
Stephano Ramos <stephano.ramos.p@gmail.com>
2021-02-22 19:54:25 -0300

    feat: content from class #1

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -8,10 +8,11 @@ defmodule Rocketpay.Numbers do
   defp handle_file({:ok, result}) do
     result = result
              |> String.split(",")
-             |> Enum.map(fn number -> String.to_integer(number) end)
+             |> Stream.map(fn number -> String.to_integer(number) end)
              |> Enum.sum()
 
     {:ok, %{result: result}}
   end
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
